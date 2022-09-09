8c5d97d681bba39bacae8262b27c4dcd3bc22922
antoonhuiskens <antoon.huiskens@gmail.com>
2022-09-09 22:39:07 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+from flask import Flask, json
+
+import requests
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def is_it_the_f5():
+    g = requests.get('https://isitthef5.com/')
+    s = BeautifulSoup(g.text, features="html.parser")
+    r = dict()
+    r['blame_f5']= s.html.body.get_text()
+    print(r)
+    return json.dumps(r)
